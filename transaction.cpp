6e8b3ee0012a933b6a42c90ddea443bc5fe9d7ae
danilboiko1302 <51995092+danilboiko1302@users.noreply.github.com>
2020-11-09 18:33:43 +0200

    transaction class

--- transaction.cpp
+++ transaction.cpp
@@ -1,6 +1,71 @@
 #include "transaction.h"
 
+QString Transaction::getDatetime() const
+{
+    return datetime;
+}
+
+void Transaction::setDatetime(const QString &value)
+{
+    datetime = value;
+}
+
+int Transaction::getAmount() const
+{
+    return amount;
+}
+
+void Transaction::setAmount(int value)
+{
+    amount = value;
+}
+
+QString Transaction::getSender() const
+{
+    return sender;
+}
+
+void Transaction::setSender(const QString &value)
+{
+    sender = value;
+}
+
+QString Transaction::getRecipient() const
+{
+    return recipient;
+}
+
+void Transaction::setRecipient(const QString &value)
+{
+    recipient = value;
+}
+
 Transaction::Transaction()
 {
 
+}
+
+Transaction::Transaction(const QString & datetime, const int amount, const QString & sender, const QString & recipient)
+{
+    this->datetime = datetime;
+    this->amount = amount;
+    this->sender = sender;
+    this->recipient = recipient;
+}
+
+Transaction::Transaction(const Transaction & a)
+{
+    this->datetime = a.datetime;
+    this->amount = a.amount;
+    this->sender = a.sender;
+    this->recipient = a.recipient;
+}
+
+Transaction &Transaction::operator=(const Transaction & a)
+{
+    this->datetime = a.datetime;
+    this->amount = a.amount;
+    this->sender = a.sender;
+    this->recipient = a.recipient;
+    return *this;
 }
--- transaction.h
+++ transaction.h
@@ -1,11 +1,27 @@
 #ifndef TRANSACTION_H
 #define TRANSACTION_H
-
+#include <QApplication>
 
 class Transaction
 {
+private:
+    QString datetime;
+    int amount;
+    QString sender;
+    QString recipient;
 public:
     Transaction();
+    Transaction(const QString&, const int, const QString&,const QString&);
+    Transaction(const Transaction&);
+    Transaction& operator=(const Transaction&);
+    QString getDatetime() const;
+    void setDatetime(const QString &value);
+    int getAmount() const;
+    void setAmount(int value);
+    QString getSender() const;
+    void setSender(const QString &value);
+    QString getRecipient() const;
+    void setRecipient(const QString &value);
 };
 
 #endif // TRANSACTION_H
